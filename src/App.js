15ca55488c9492ff75e41550a946c43d401a1845
dymafr <erwan1309@hotmail.com>
2022-07-04 14:46:51 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import Content from './pages/HomePage/HomePage';
+import HomePage from './pages/HomePage/HomePage';
 import styles from './App.module.scss';
 
 function App() {
   return (
     <div className={`d-flex flex-column ${styles.appContainer}`}>
       <Header />
-      <Content />
+      <HomePage />
       <Footer />
     </div>
   );
